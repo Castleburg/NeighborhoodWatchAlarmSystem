a95dd7273f5b28ce2e8d83d3c67085fe4a0eb12c
Christian <Christian-herreborg@hotmail.com>
2019-02-25 12:19:53 +0100

    derp

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	//Starts the program
 	public static void main(String[] args){
 		//Create controller and start server hosting
-		Controller controller = new Controller(6060);
+		Controller controller = new Controller(6161);
 		try {
 			controller.startServer();
 		} catch (InterruptedException e) {
