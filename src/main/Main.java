ecbea95d25e06ad263ad78905c94cdf863797d5c
Christian <Christian-herreborg@hotmail.com>
2019-02-25 12:29:17 +0100

    ha

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ import main.Controller;
 public class Main {
 	//Starts the program
 	public static void main(String[] args){
+		System.out.println("Hello from Main");
 		//Create controller and start server hosting
 		Controller controller = new Controller(6161);
 		try {
